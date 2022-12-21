7c73305cb844ef5695babdcb332a0450a2cd54ad
andrwcao <80874259+andrwcao@users.noreply.github.com>
2022-12-21 20:43:46 +1100

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push andrwcao/multi-worker:$SHA
 
 kubectl apply -f k8s
 
-kubectl set image deployments/client-deployment server=andrwcao/multi-client:$SHA
-kubectl set image deployments/server-deployment client=andrwcao/multi-server:$SHA
+kubectl set image deployments/client-deployment client=andrwcao/multi-client:$SHA
+kubectl set image deployments/server-deployment server=andrwcao/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=andrwcao/multi-worker:$SHA
